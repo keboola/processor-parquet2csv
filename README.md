ee317ff9000c297babf48798754f0630b86793c6
Samuel Kozuch <samuel.kozuch@keboola.com>
2020-04-29 19:21:58 +0200

    FEAT: Added fill and strict mode DOCS: Added documentation

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Parquet Processor
 
+Parquet processor utilizes the [pyarrow](https://pypi.org/project/pyarrow/) library to process Parquet files and convert them into a single .csv file. A single configuration, in which the Parquet processor is used, should contain the files which belong to one table only and thus maintaining one configuration, one table rule.
+
 ## Confifuration:
 
-A sample configuration of the processor:
+A sample of the configuration object:
 
 ```json
 {
@@ -21,6 +23,7 @@ A sample configuration of the processor:
         "chunk_size": 5000,
         "columns": [
             "id",
+            "order_id",
             "dwh_created",
             "created_at"
         ]
@@ -28,11 +31,39 @@ A sample configuration of the processor:
 }
 ```
 
-### Parameters
+## Parameters
 
-- `mode` - For now, only `fast` option is supported. Further options will be added in the future. This parameters is required.
-- `table_name` - A name of the table in storage.
-- `incremental` - A boolean value marking, whether to utilize incremental storage.
-- `primary_keys` - An array of primary keys.
-- `include_filename` - A boolean value. If `true`, an extra column with name of data parquet origin file will be included in the output table.
-- `chunk_size` - A positive integer specifying the size of a chunk, which should be processed in memory. In general, chunk size means lower memory consumption, but slower processing. If `chunk_size` is not specified or a negative number, the whole file is processed in memory without batching.
+- `mode` (required) - A mode, which will be utilized in running of the processor. Must be one of `fast`, `fill` or `strict`. See below for further clarification.
+- `table_name` (required) - A name of the table in storage.
+- `incremental` (optional) - A boolean value marking, whether to utilize incremental load to storage. If not specified, full load is performed.
+- `primary_keys` (optional) - An array of primary keys.
+- `include_filename` (optional) - A boolean value. If `true`, an extra column `parquet_filename` with name of data parquet origin file will be included in the output table. Default is `false`.
+- `chunk_size` (optional) - A positive integer specifying the size of a chunk, which should be processed in memory. In general, the lower the chunk size, the lower memory consumption, but slower processing and vice versa. If `chunk_size` is not specified or is a negative number, the whole file is processed in memory without batching. Default is no batching used.
+- `columns` (optional) - An array of columns, which will be read from the Parquet file. If any of the columns specified here is not present in the Parquet file, it will be ignored. If mode is set to `strict`, this parameter is required, since it defines the schema, which should be checked.
+- `debug` - A boolean value. If `true`, extra logging is added. Default is `false`.
+
+### Different `mode` parameter specification
+
+Since the processor will be processing files continuously, some change in schema of Parquet files may occur with different exports (e.g. new column added or removed). The processor offers different modes to treat the issue that may arise.
+
+#### Mode `fast`
+
+The `fast` mode tries to be as fast in processing files as possible. It determines the schema from the first Parquet file it reads and applies to schema to all remaining Parquet files which are processed, regardless of what is their actual schema. This process is fast, but may lead to some data being lost, if schema of processed files is different.
+If parameter `columns` is specified, the processor will read only specified columns and apply them to followin files.
+
+#### Mode `fill`
+
+The `fill` mode reads the schemas of all files in the processing queue and creates a singular schema which is applied to all files. If some columns are missing in one of the files, those will be filled with blanks.
+
+#### Mode `strict`
+
+Strict mode makes sure that all of the files adhere to the same schema, which is defined by `columns` parameters. If any of the columns defined in `columns` parameters is missing in one of the files, an error is raised and conversion to a csv is halted.
+
+## Development
+
+To build and run a docker image, use following commands:
+
+```
+docker-compose build dev
+docker-compose run --rm dev
+```
--- src/component.py
+++ src/component.py
@@ -1,5 +1,3 @@
-import csv
-import fastparquet
 import glob
 import json
 import logging
@@ -18,7 +16,7 @@ KEY_CHUNKSIZE = 'chunk_size'
 KEY_DEBUG = 'debug'
 
 MANDATORY_PARAMETERS = [KEY_MODE, KEY_TABLE_NAME]
-SUPPORTED_MODES = ["fast"]  # , "fill", "script"]
+SUPPORTED_MODES = ["fast", "fill", "strict"]
 
 FILENAME_COLUMN = 'parquet_filename'
 
@@ -80,7 +78,7 @@ class ParquetParser(KBCEnvHandler):
             logging.error("Parameter \"columns\" must be of type list.")
             sys.exit(1)
 
-        elif self.par_table_columns == []:
+        elif len(self.par_table_columns) == 0:
             self.par_table_columns = None
 
         else:
@@ -105,7 +103,6 @@ class ParquetParser(KBCEnvHandler):
                 sys.exit(1)
 
             self.par_chunk_size = _cs if _cs > 0 else None
-            logging.debug(f"Chunk size set to {self.par_chunk_size}.")
 
     def getParquetFiles(self):
 
@@ -125,8 +122,18 @@ class ParquetParser(KBCEnvHandler):
 
         if self.par_mode == 'fast':
             _columns = self._fastProcess(path_table)
-        self.createManifest(path_table, columns=_columns)
 
+        elif self.par_mode == 'fill':
+            _columns = self._fillProcess(path_table)
+
+        elif self.par_mode == 'strict':
+            _columns = self._strictProces(path_table)
+
+        else:
+            logging.error(f"Unsupported mode {self.par_mode}.")
+            sys.exit(1)
+
+        self.createManifest(path_table, columns=_columns)
         logging.info(f"Converted {len(self.var_pq_files_names)} Parquet files to csv.")
 
     def createManifest(self, table_path, columns):
@@ -156,6 +163,18 @@ class ParquetParser(KBCEnvHandler):
                 if schema is None:
                     schema = _pq_file.schema
 
+                    if schema.names == [] and self.par_table_columns is not None:
+                        logging.error("Schema is empty. Make sure parameter \"columns\" specifies correct columns " +
+                                      "present in schema.")
+                        sys.exit(1)
+
+                    elif schema.names == []:
+                        logging.error("Schema is empty.")
+                        sys.exit(1)
+
+                    else:
+                        pass
+
                     logging.debug(f"Using following schema to parse the files:\n{schema}.")
 
                     if self.par_table_columns is None:
@@ -172,17 +191,13 @@ class ParquetParser(KBCEnvHandler):
                 _pq_batches = _pq_file.to_batches(max_chunksize=self.par_chunk_size)
 
                 for _pq_batch in _pq_batches:
-
                     _df_batch = _pq_batch.to_pandas()
 
                     for _c in columns:
 
                         if _c not in _df_batch.columns:
-                            if _c == FILENAME_COLUMN and self.par_include_filename is True:
+                            if _c == FILENAME_COLUMN:
                                 _df_batch[_c] = filename
-
-                            elif _c == FILENAME_COLUMN and self.par_include_filename is False:
-                                continue
 
                             else:
                                 _df_batch[_c] = ''
@@ -193,63 +208,83 @@ class ParquetParser(KBCEnvHandler):
 
         return columns
 
-    def _fastProcess_pyarrow(self, table_path):
+    def _fillProcess(self, table_path):
 
-        _wrt = None
+        schema_columns = []
 
-        with open(table_path, 'w') as out_table:
+        for path in self.var_pq_files_paths:
 
-            for f, filename in zip(self.files, self.files_clean):
-                _pq = pq.read_table(f)
-                pq_batches = _pq.to_batches(max_chunksize=5000)
-                parquet_columns = _pq.schema.names
-                all_cols = parquet_columns if self.__columns is None else self.__columns
-                if self.__filename is True:
-                    all_cols += [FILENAME_COLUMN]
+            _pq_file = pq.read_table(path, columns=self.par_table_columns)
+            schema_columns += _pq_file.schema.names
 
-                if _wrt is None:
-                    _wrt = csv.DictWriter(out_table, fieldnames=all_cols,
-                                          restval='', extrasaction='ignore',
-                                          quotechar='\"', quoting=csv.QUOTE_ALL)
-                    _wrt.writeheader()
+        schema_columns = list(set(schema_columns))
+        logging.debug(schema_columns)
 
-                for _b in pq_batches:
-                    pdb = _b.to_pandas().fillna('')
+        if self.par_include_filename is True:
+            schema_columns += [FILENAME_COLUMN]
 
-                    for _, row in pdb.iterrows():
-                        _wrt.writerow(row)
+        with open(table_path, 'w') as out_results:
 
-    def _fastProcess_deprecated(self, table_path, manifest_path):
+            for path, filename in zip(self.var_pq_files_paths, self.var_pq_files_names):
 
-        _wrt = None
+                _pq_file = pq.read_table(path, columns=schema_columns)
+                _pq_batches = _pq_file.to_batches(max_chunksize=self.par_chunk_size)
 
-        with open(table_path, 'w') as out_table:
+                for _pq_batch in _pq_batches:
 
-            for f, filename in zip(self.files, self.files_clean):
-                pq = fastparquet.ParquetFile(f)
-                parquet_columns = pq.columns
+                    _df_batch = _pq_batch.to_pandas()
 
-                all_cols = parquet_columns if self.__columns is None else self.__columns
-                if self.__filename is True:
-                    all_cols += [FILENAME_COLUMN]
+                    for _c in schema_columns:
 
-                if _wrt is None:
-                    _wrt = csv.DictWriter(out_table, fieldnames=all_cols,
-                                          restval='', extrasaction='ignore',
-                                          quotechar='\"', quoting=csv.QUOTE_ALL)
+                        if _c not in _df_batch.columns:
+                            if _c == FILENAME_COLUMN:
+                                _df_batch[_c] = filename
 
-                try:
-                    df = pq.to_pandas().fillna('')
-                    if self.__filename is True:
-                        df[FILENAME_COLUMN] = filename
-                    # df[all_cols].to_csv(table_path, mode='a', header=False, index=False)
+                            else:
+                                _df_batch[_c] = ''
 
-                    for _, row in df.iterrows():
-                        _wrt.writerow(row)
+                    _df_batch[schema_columns].to_csv(out_results, header=False, index=False, na_rep='')
 
-                except ValueError as e:
-                    logging.exception(f"Exception encountered when converting parquets to a csv. {e}")
+                logging.debug(f"Converted {filename} to csv. Rows: {_pq_file.num_rows}.")
+
+        return schema_columns
+
+    def _strictProces(self, table_path):
+
+        if self.par_table_columns is None:
+            logging.error("Parameter \"columns\" must be specified for strict mode.")
+            sys.exit(1)
+
+        columns = self.par_table_columns
+        if self.par_include_filename is True:
+            columns += [FILENAME_COLUMN]
+
+        with open(table_path, 'w') as out_results:
+
+            for path, filename in zip(self.var_pq_files_paths, self.var_pq_files_names):
+
+                _pq_file = pq.read_table(path)
+
+                missing_columns = list(set(columns) - set(_pq_file.schema.names) - set([FILENAME_COLUMN]))
+                if missing_columns != []:
+                    logging.error(f"Missing columns {missing_columns} in file {filename}, which were defined " +
+                                  f"in configuration parameter \"columns\".\n" +
+                                  f"Available columns are {_pq_file.schema.names}.")
                     sys.exit(1)
+
+                _pq_batches = _pq_file.to_batches(max_chunksize=self.par_chunk_size)
+
+                for _pq_batch in _pq_batches:
+
+                    _df_batch = _pq_batch.to_pandas()
+                    if FILENAME_COLUMN in columns:
+                        _df_batch[FILENAME_COLUMN] = filename
+
+                    _df_batch[columns].to_csv(out_results, header=False, index=False, na_rep='')
+
+                logging.debug(f"Converted {filename} to csv. Rows: {_pq_file.num_rows}.")
+
+        return columns
 
     def run(self):
         self.getParquetFiles()
